21af68c40c0c42bfbd882af7806fb386028d97f3
Nhi Huynh <nhi.huynh@hotmail.com>
2025-09-13 07:43:19 +0800

    feat: create a breakfastmenucontainer parent element

--- src/domstuff.js
+++ src/domstuff.js
@@ -18,3 +18,39 @@ function paragraphTagUpdater(){
 
 // call the function to update the p tag
 paragraphTagUpdater();
+
+
+
+// Breakfast Menu renderer
+
+// array of breakfast menu items
+let breakfastMenuItems = [
+	"pancakes",
+	"blueberry pancakes",
+	"bacon and eggs",
+	"omelette",
+	"avo smash"
+];
+
+// function to render the breakfast menu
+function breakfastRenderer () {
+	// 1. render the breakfast menu container
+	// 1a. find the menu container's parent element
+	let targetParentContainer = document.querySelector("body");
+	// 1b. create the menu container element
+	let menuContainer = document.createElement("section");
+	console.log(menuContainer);
+	menuContainer.id = "breakfastmenu";
+	// 1c. append the menu container element to the parent element
+	targetParentContainer.appendChild(menuContainer);
+	// document.body.appendChild()
+
+
+	// 2. create menu options
+
+	// 3. insert menu options into breakfast menu container
+}
+
+// call the breakfast menu renderer function
+breakfastRenderer();
+
